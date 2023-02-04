abd1b939f789c9850d8f1a762d973f07afe5865e
YuriiCodes <yuriipidlisnyi2021@gmail.com>
2023-02-04 17:25:45 +0200

    Added login & register form mocks

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -2,12 +2,16 @@
 import styles from './app.module.css';
 import Home from "../pages/Home";
 import {Route, Routes} from "react-router-dom";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 export function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
       </Routes>
     </>
   );
---
+++ apps/frontend/src/pages/Login.tsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div className={'container mx-auto px-auto'}>
+      Login
+    </div>
+  )
+};
+
+export default Login;
