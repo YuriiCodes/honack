c6a42f525867bfcb82ca877eeb50e032f2d467c5
YuriiCodes <yuriipidlisnyi2021@gmail.com>
2023-02-04 17:33:53 +0200

    Updated register form

--- apps/frontend/src/pages/Register.tsx
+++ apps/frontend/src/pages/Register.tsx
@@ -1,7 +1,27 @@
 const Register = () => {
   return (
     <div>
-      Register
+      <form>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Your Email</span>
+          </label>
+          <label className="input-group">
+            <span>Email</span>
+            <input type="text" placeholder="info@site.com" className="input input-bordered" />
+          </label>
+        </div>
+
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Your username</span>
+          </label>
+          <label className="input-group">
+            <span>Username</span>
+            <input type="text" placeholder="Test user" className="input input-bordered" />
+          </label>
+        </div>
+      </form>
     </div>
   )
 };
