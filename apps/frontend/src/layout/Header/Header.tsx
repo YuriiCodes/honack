4ddc75f11ac65f2cb4cc711b55af9d173fd3bac4
YuriiCodes <yuriipidlisnyi2021@gmail.com>
2023-03-12 15:53:35 +0200

    Added formik to the registration form

--- apps/frontend/src/layout/Header/Header.tsx
+++ apps/frontend/src/layout/Header/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-base-300">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
--- apps/frontend/src/pages/Register.tsx
+++ apps/frontend/src/pages/Register.tsx
@@ -1,29 +1,82 @@
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+
+const SignupSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string().required("Required").min(8, "Too short").max(50, "Too long")
+});
+
+
 const Register = () => {
   return (
-    <div>
-      <form>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Your Email</span>
-          </label>
-          <label className="input-group">
-            <span>Email</span>
-            <input type="text" placeholder="info@site.com" className="input input-bordered" />
-          </label>
-        </div>
+    <div className={"container mx-auto flex justify-center h-screen"}>
+      <Formik
+        initialValues={{
+          username: "",
+          email: "",
+          password: ""
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={values => {
+          // same shape as initial values
+          console.log(values);
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Your Email</span>
+              </label>
+              <label className="input-group" htmlFor={"email"}>
+                <span>Email</span>
+                <div className={"flex justify-center"}>
+                  <Field type="text" id="email" name={"email"} placeholder="info@site.com"
+                         className="input input-bordered" />
+                  {errors.email && touched.email ? (
+                    <div className={"text-orange-700"}>{errors.email}</div>
+                  ) : null}
+                </div>
+              </label>
+            </div>
 
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Your username</span>
-          </label>
-          <label className="input-group">
-            <span>Username</span>
-            <input type="text" placeholder="Test user" className="input input-bordered" />
-          </label>
-        </div>
-      </form>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Your username</span>
+              </label>
+              <label className="input-group">
+                <span>Username</span>
+                <Field type="text" name={"username"} placeholder="Test user" className="input input-bordered" />
+                {errors.username && touched.username ?
+                  <div className={"text-orange-700"}>{errors.username}</div> : null}
+              </label>
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Your password</span>
+              </label>
+              <label className="input-group" htmlFor={"password"}>
+                <span>password</span>
+                <div className={"flex justify-center"}>
+                  <Field type="password" id="password"  name="password" className="input input-bordered" />
+                  {errors.password && touched.password ? (
+                    <div className={"text-orange-700"}>{errors.password}</div>
+                  ) : null}
+                </div>
+              </label>
+            </div>
+            <div className="form-control mt-3">
+              <button className="btn btn-outline">Sign up</button>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
-  )
+  );
 };
 
 export default Register;
